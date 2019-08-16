7820415e74d7ebbbc208d978c59324a5d09e9799
Tom Davies <tom@thenuel.com>
2019-08-16 22:04:21 +0100

    Adds CloudFormation template for publishing

---
+++ deploy/scripts/build-layer.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# remove old versions
+rm -rf dist/
+
+# installing locally will just symlink, which we can't upload -> so use registry
+npm install --prefix dist/nodejs @nuel/fastify-next-lambda@latest
+
+# lambda doesn't like scoped handlers, so remove the @nuel scope folder
+cp -r dist/nodejs/node_modules/@nuel/fastify-next-lambda dist/nodejs/node_modules
+
+# ...then cleanup
+rm -rf dist/nodejs/node_modules/@nuel
