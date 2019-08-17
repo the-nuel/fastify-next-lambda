5eb2a239155872102a223ecfde84ab1fbed37569
Tom Davies <tom@thenuel.com>
2019-08-17 19:20:53 +0100

    Adds support for setting cache-control headers

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -32,7 +32,7 @@ describe('handler', () => {
             },
         });
     });
-    
+
     it('support dynamic routing', async () => {
         const response = await handler({
             httpMethod: 'GET',
@@ -63,5 +63,42 @@ describe('handler', () => {
                 date: expect.any(String),
             },
         });
+    });
+});
+
+describe('caching', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    afterEach(() => {
+        delete process.env.STATIC_DEFAULT_MAX_AGE;
+        delete process.env.DYNAMIC_DEFAULT_MAX_AGE;
+    });
+
+    it('defaults cache-control headers for static files to 0s', async () => {
+        const handler = require('../index').handler;
+        const response = await handler({ httpMethod: 'GET', path: '/' });
+        expect(response.headers['cache-control']).toBe('public, max-age=0');
+    });
+
+    it('allows cache-control header duration for static files to be set', async () => {
+        process.env.STATIC_DEFAULT_MAX_AGE = '60';
+        const handler = require('../index').handler;
+        const response = await handler({ httpMethod: 'GET', path: '/' });
+        expect(response.headers['cache-control']).toBe('public, max-age=60');
+    });
+
+    it('defaults cache-control headers for dynamic files to 0s', async () => {
+        const handler = require('../index').handler;
+        const response = await handler({ httpMethod: 'GET', path: '/about' });
+        expect(response.headers['cache-control']).toBe('public, max-age=0');
+    });
+
+    it('allows cache-control header duration for dynamic files to be set', async () => {
+        process.env.DYNAMIC_DEFAULT_MAX_AGE = '30';
+        const handler = require('../index').handler;
+        const response = await handler({ httpMethod: 'GET', path: '/about' });
+        expect(response.headers['cache-control']).toBe('public, max-age=30');
     });
 });
--- src/utils/buildNextRoutes.ts
+++ src/utils/buildNextRoutes.ts
@@ -3,7 +3,13 @@ import send from 'send';
 import { ServerResponse } from 'http';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
+function loadIntegerWithFallback(key: string, fallback: string = '0') {
+    return parseInt(process.env[key] || fallback);
+}
+
 const NEXT_SERVERLESS_ROOT = path.join(process.cwd(), '.next', 'serverless');
+const STATIC_DEFAULT_MAX_AGE = loadIntegerWithFallback('STATIC_DEFAULT_MAX_AGE');
+const DYNAMIC_DEFAULT_MAX_AGE = loadIntegerWithFallback('DYNAMIC_DEFAULT_MAX_AGE');
 
 const manifest: { [key: string]: string } = require(path.join(
     NEXT_SERVERLESS_ROOT,
@@ -14,7 +20,10 @@ export const buildStaticHandler = (page: string) => {
     return (req: FastifyRequest, reply: FastifyReply<ServerResponse>) => {
         const file = path.join(NEXT_SERVERLESS_ROOT, page);
         req.log.info({ file }, 'serving static page');
-        send(req.raw, file).pipe(reply.res);
+
+        send(req.raw, file, { maxAge: STATIC_DEFAULT_MAX_AGE * 1000 }).pipe(
+            reply.res
+        );
     };
 };
 
@@ -22,6 +31,11 @@ const buildDynamicHandler = (page: string) => {
     return (req: FastifyRequest, reply: FastifyReply<ServerResponse>) => {
         const file = path.join(NEXT_SERVERLESS_ROOT, page);
         req.log.info({ file }, 'serving dynamic page');
+
+        reply.res.setHeader(
+            'cache-control',
+            `public, max-age=${DYNAMIC_DEFAULT_MAX_AGE}`
+        );
 
         require(file)
             .render(req.raw, reply.res)
@@ -37,7 +51,7 @@ export function buildNextRoutes() {
         .map(path => {
             const target = manifest[path];
             return {
-                route: path.replace(/\[([a-z0-9]+)\]/gi, ":$1"),
+                route: path.replace(/\[([a-z0-9]+)\]/gi, ':$1'),
                 handler: target.endsWith('.html')
                     ? buildStaticHandler(manifest[path])
                     : buildDynamicHandler(manifest[path]),
